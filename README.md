34fdbe15bebbce50895e6123bba762ec25f83abd
Mark Jordan <mjordan@sfu.ca>
2018-11-03 18:24:53 +0000

    Added get_fedora_uris command.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Islandora Console
 
-Some horsing around to do with https://github.com/Islandora-CLAW/CLAW/issues/965.
+Proof of concept implementation of Islandora CLAW replacement for Islandora 7.x's [Datastream CRUD](https://github.com/SFULibrary/islandora_datastream_crud). See https://github.com/Islandora-CLAW/CLAW/issues/965 for more details.
 
 ## Requirements
 
@@ -13,21 +13,24 @@ Some horsing around to do with https://github.com/Islandora-CLAW/CLAW/issues/965
 
 ## Usage
 
-Only one command so far:
+Two commands so far:
 
 * `drupal islandora_console:print_title --nid_file=PATH_TO_FILE`
    * where `PATH_TO_FILE` is the absolute path to a file containing one node ID per line, e.g. `drupal islandora_console:print_title --nid_file=/tmp/node_ids.txt`
+   * prints the title of the nodes, which is not that useful, it's just sort of a "hello world" command.
+* `drupal islandora_console:get_fedora_uris --nid_file=PATH_TO_FILE`
+   * where `PATH_TO_FILE` is the absolute path to a file containing one node ID per line, e.g. `drupal islandora_console:get_fedora_uris --nid_file=/tmp/node_ids.txt`
 
-This will print the title of each of the nodes identified in the file. If a node does not exist, a warning message will appear. If the file does not exist, a different warning message will appear:
+This will print the node ID and the corresponding Fedora URI for the node, separted by a comma:
 
 ```
-vagrant@claw:/var/www/html/drupal$ drupal islandora_console:print_title --nid_file=/tmp/nids.txt
- [OK] Winter Travel in the North, Atlin, B.C.                                                                           
- [OK] Ruskin Power Plant, near Mission City, B.C.                                                                       
-vagrant@claw:/var/www/html/drupal$ 
+drupal islandora_console:get_fedora_uris --nid_file=/tmp/nodes.txt
+90,http://localhost:8080/fcrepo/rest/b1/8b/f3/be/b18bf3be-9615-47ca-bb4e-6fe4d97da2a6
+92,http://localhost:8080/fcrepo/rest/55/40/43/23/55404323-81c5-4096-8210-c57bcab77cf8
+93,http://localhost:8080/fcrepo/rest/73/f5/d9/f1/73f5d9f1-fabc-4a01-8b5c-8e764eb9060c
 ```
 
-This command is a simple example only. We could run an action against the node, such as generate a thumbnail from a service file.
+For both commands, if a node does not exist, a warning message will appear. If the file specified in `--nid_file` does not exist, a warning message to that effect will appear.
 
 ## Current maintainer
 
---
+++ src/Command/GetFedoraUrisCommand.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\islandora_console\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Drupal\Console\Core\Command\ContainerAwareCommand;
+use Drupal\Console\Annotations\DrupalCommand;
+
+/**
+ * Class PrintTitleCommand.
+ *
+ * @DrupalCommand (
+ *     extension="islandora_console",
+ *     extensionType="module"
+ * )
+ */
+class GetFedoraUrisCommand extends ContainerAwareCommand {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    $this
+      ->setName('islandora_console:get_fedora_uris')
+      ->setDescription($this->trans('commands.islandora_console.print_title.description'))
+      ->addOption('nid_file', NULL, InputOption::VALUE_REQUIRED, $this->trans('commands.islandora_console.print_tilte.options.nid_file'), NULL);
+
+    // @todo: Add options for specifying the Gemini base URL and authorization credentials.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $nid_file_path = $input->getOption('nid_file');
+    if (file_exists($nid_file_path)) {
+      $nids = file($nid_file_path, FILE_IGNORE_NEW_LINES);
+      foreach ($nids as $nid) {
+        if ($node = \Drupal::service('entity_type.manager')->getStorage('node')->load($nid)) {
+          $uuid = $node->uuid->value;
+          $gemini_results = $this->query_gemini($uuid);
+          print $nid . "," . $gemini_results['fedora'] . "\n";
+        }
+        else {
+          $this->getIo()->warning($this->trans('commands.islandora_console.get_fedora_uris.messages.cannotfindnode'));
+        }
+      }
+    }
+    else {
+      $this->getIo()->warning($this->trans('commands.islandora_console.get_fedora_uris.messages.cannotfindfile'));
+    }
+  }
+
+  /**
+   * Queries Gemini's REST API for node's the Fedora URI.
+   *
+   * @param string $uuid
+   *   The node's UUID.
+   *
+   * @return arrray
+   *   The results of the request as an associative array with keys 'drupal' and 'fedora'.
+   */
+  protected function query_gemini($uuid) {
+    $url = 'http://localhost:8000/gemini/' . $uuid;
+    $response = \Drupal::httpClient()->get($url, ['headers' => array('Authorization' => 'Bearer islandora')]);
+    $response_body = (string) $response->getBody();
+    $response_body = json_decode($response_body, true);
+    return $response_body;
+  }
+}
